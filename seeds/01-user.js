7c882a34f1ee6545587704ea3c8b899c3a7c1043
Varga Veronika <varga.veronika@hallgato.sze.hu>
2024-09-11 21:18:49 +0200

    UPDATE - help

--- seeds/01-user.js
+++ seeds/01-user.js
@@ -4,14 +4,23 @@
  */
 exports.seed = async function (knex) {
     await knex("user").del();
+    await knex("timetable").insert([  //create dev user
+      {
+        username: "tanar",
+        name: "teszt tanar",
+        password: "tanar",
+        email: "tanar@email.com",
+        created_at: new Date(),
+      },
+    ]);
+
     await knex("user").insert([  //create dev user
       {
-        uuid: "aa87270e-3eff-40c3-ab5b-9b3d940a3806",
-        email: "dev",
-        name: "dev",
-        password: "5fd7087e3ba5e067d8c154a716949ebb7a158a5ecceaec1674432d7f36fb9fa9699dadfa571ac87ed752c6c9297a4f50f03e16c2925a5411b98ee4a29bdacced",
+        username: "tanar",
+        name: "teszt tanar",
+        password: "tanar",
+        email: "tanar@email.com",
         created_at: new Date(),
-        updated_at: new Date(),
       },
     ]);
   };
